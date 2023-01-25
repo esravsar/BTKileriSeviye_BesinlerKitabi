c910a7311f7b591c9cf4b3164f183f1a5985483d
EsraAvsar <50179587+esravsar@users.noreply.github.com>
2023-01-25 19:13:34 +0300

    Navigation yapısı kuruldu

--- app/src/main/java/esra/avsar/besinlerkitabi/BesinDetayiFragment.kt
+++ app/src/main/java/esra/avsar/besinlerkitabi/BesinDetayiFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_besin_detayi.*
 
 class BesinDetayiFragment : Fragment() {
+
+    private var besinId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +22,19 @@ class BesinDetayiFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_besin_detayi, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            besinId = BesinDetayiFragmentArgs.fromBundle(it).besinId
+            println(besinId)
+        }
+
+        besin_detayi_button.setOnClickListener {
+            val action = BesinDetayiFragmentDirections.actionBesinDetayiFragmentToBesinListesiFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
--- app/src/main/java/esra/avsar/besinlerkitabi/BesinListesiFragment.kt
+++ app/src/main/java/esra/avsar/besinlerkitabi/BesinListesiFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_besin_listesi.*
 
 class BesinListesiFragment : Fragment() {
 
@@ -18,5 +20,15 @@ class BesinListesiFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_besin_listesi, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        besin_listesi_button.setOnClickListener {
+            val action = BesinListesiFragmentDirections.actionBesinListesiFragmentToBesinDetayiFragment()
+            action.besinId = 3
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
